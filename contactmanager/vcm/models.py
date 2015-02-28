5ea926c6e232aed4a82a13ca2b646171275821ae
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2015-02-28 10:19:07 +0300

    Alpha contact model

--- contactmanager/vcm/models.py
+++ contactmanager/vcm/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    # names, ;-separated
+    family_name = models.CharField(max_length=200)
+    # from vCard
+    display_name = models.CharField(max_length=200)
+    # birth date, in format YYYY-MM-DD
+    bday = models.CharField(max_length=12)
+    email = models.CharField(max_length=64)
+    tel_mobile = models.CharField(max_length=20)
+    tel_work = models.CharField(max_length=20)
+    tel_home = models.CharField(max_length=20)
+    pub_date = models.DateTimeField('date published')
+
--- contactmanager/vcm/views.py
+++ contactmanager/vcm/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+"""
+VCM Views
+"""
+#from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+
+    return HttpResponse("QQQ")
