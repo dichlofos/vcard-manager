c3ac98f89d4b2e14cc4d3bfe70a40da1c70d0257
Mikhail Veltishchev <mvel@yandex-team.ru>
2015-03-05 00:34:59 +0300

    More crap

--- contactmanager/vcm/views.py
+++ contactmanager/vcm/views.py
@@ -32,6 +32,7 @@ def load(request):
     # load
     all_vcards = open('../final-filtered.vcf').read()
     cards = vobject.readComponents(all_vcards)
+    count = 0
     for card in cards:
         card_db = Card()
         #print vobject.vcard.Name.toString(card.n.value)  ##. value.encode('utf-8')
@@ -40,16 +41,21 @@ def load(request):
         #print card.fn.value.encode('utf-8')
         #card.prettyPrint()
         #print card.serialize(lineLength=75)
+        count = count + 1
+        if count > 100:
+            break
+
         if getattr(card, "tel", None) is not None:
-            card_db.family_name = card.fn
+            #card_db.family_name = card.contents['fn'].value.encode('utf-8')  #[0]  # .encode('utf-8')
             for key in card.contents:
-                if key not in ['version', 'n', 'fn', 'photo']:
+                if key not in ['version', 'fn', 'photo']:
                     # кошерно, содержит все повторения атрибута (списком)
                     values = card.contents[key]
                     for value in values:
-                        print value
+                        print type(value.value)
+                        print value.serialize()
                     #print card.contents[key]
-            card_db.save()
+            #card_db.save()
         #else:
         #    print card.tel
 
